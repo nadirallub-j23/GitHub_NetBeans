43a3704cb6197daf3d018f5766cfd1d3645424a9
user <user@10.10.201.242>
2026-01-13 18:04:34 +0100

    Añadida mi clase y la llamada de mi clase en menú

--- src/ejemploProyecto/EjemploProyecto.java
+++ src/ejemploProyecto/EjemploProyecto.java
@@ -4,6 +4,8 @@
  */
 package ejemploProyecto;
 
+import java.util.Scanner;
+
 /**
  * Clase principal de cuando creamos el proyecto
  * @author Nadir
@@ -16,7 +18,21 @@ public class EjemploProyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         Scanner teclado = new Scanner(System.in);
+        JorgeClass jorge = new JorgeClass();
         
+        System.out.print("Introduce la primera nota: ");
+        float n1 = teclado.nextFloat();
+        System.out.print("Introduce la segunda nota: ");
+        float n2 = teclado.nextFloat();
+        System.out.print("Introduce la tercera nota: ");
+        float n3 = teclado.nextFloat();
+        
+        float promedio = jorge.promedioNota(n1, n2, n3);
+        String mensaje = jorge.conclusionFinal(promedio);
+        
+        System.out.println("Promedio obtenido: " + promedio);
+        System.out.println("Conclusion final: " + mensaje);
         // comentario de ejemplo primera rama
         
         
